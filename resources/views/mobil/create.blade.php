bc56f31ab5b261b7204937909a3468c191149e2e
Muhammad Gibran Pangestu <gibranp902@gmail.com>
2026-06-25 17:00:51 +0700

    fix: perbaiki logika CustomerController

--- resources/views/mobil/create.blade.php
+++ resources/views/mobil/create.blade.php
@@ -68,7 +68,7 @@
                     <div class="space-y-2">
                         <label class="text-sm font-medium text-white/80">Harga Sewa / Hari</label>
                         <div class="relative">
-                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-white/40 text-sm">Rp</span>
+                            <span class="absolute left-3 top-1/2 -translate-y-1/2 text-white/40 text-sm"></span>
                             <input type="number" name="harga_mobil" value="{{ old('harga_mobil') }}" required min="0" placeholder="500000" class="w-full h-10 rounded-lg border border-white/[0.1] bg-[#0D0D0D] text-white pl-10 pr-3 text-sm outline-none transition-colors placeholder:text-white/40 focus:border-[#C1121F]/50 focus:shadow-[0_0_0_2px_rgba(193,18,31,0.3)]">
                         </div>
                     </div>
